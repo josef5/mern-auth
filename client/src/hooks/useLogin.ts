cc56362d535ab277fe190241b5187b17350a2abe
Jose Espejo <joseespejo@yahoo.com>
2024-03-03 20:02:57 +0000

    feat: add useLogin

---
+++ client/src/hooks/useLogin.ts
@@ -0,0 +1,45 @@
+import { useState } from "react";
+import useAuth from "./useAuth";
+
+const useLogin = () => {
+  const [isFetching, setIsFetching] = useState(false);
+  const { setAuth } = useAuth();
+  const [error, setError] = useState("");
+
+  const login = async (username: string, password: string) => {
+    console.log({ username, password });
+
+    setIsFetching(true);
+
+    try {
+      const response = await fetch("/auth/login", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ username, password }),
+      });
+
+      const data = await response.json();
+      // console.log("data :", data);
+
+      if (!response.ok) {
+        throw new Error(data.message);
+      }
+
+      setAuth({ username: data.username, isAuthenticated: true });
+      setIsFetching(false);
+      setError("");
+    } catch (error) {
+      console.error(error);
+
+      setError("There was an error logging in. Please try again.");
+      setAuth({ username: "", isAuthenticated: false });
+      setIsFetching(false);
+    }
+  };
+
+  return { isFetching, error, login };
+};
+
+export default useLogin;
