70fa88acddeaad4cd8cf10c5d8e188efc9b28e48
Jose Espejo <joseespejo@yahoo.com>
2024-03-03 20:02:38 +0000

    feat: check auth state on first load

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,4 +1,45 @@
+import { useEffect } from "react";
+import useAuth from "../hooks/useAuth";
+
 const Home = () => {
+  const { auth, setAuth } = useAuth();
+  console.log("auth :", auth);
+
+  useEffect(() => {
+    const fetchAuthStatus = async () => {
+      // setIsFetching(true);
+
+      try {
+        const response = await fetch("/auth/status");
+        const data = await response.json();
+        // console.log("data :", data);
+
+        // await sleep(2000);
+
+        if (!response.ok) {
+          throw new Error(data.message);
+        }
+
+        // setIsAuth(data.isAuthenticated);
+        setAuth({
+          username: data.username,
+          isAuthenticated: data.isAuthenticated,
+        });
+        // setIsFetching(false);
+        // setIsError(false);
+      } catch (error) {
+        console.error(error);
+
+        // setIsError(true);
+        // setIsAuth(false);
+        setAuth({ username: "", isAuthenticated: false });
+        // setIsFetching(false);
+      }
+    };
+
+    fetchAuthStatus();
+  }, [setAuth]);
+
   return (
     <div>
       <h2>Home</h2>
